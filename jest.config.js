73f768704e3bf76edb0c9f4ea21ea2fc0515ee6e
Harry Park <harry@harrypark.io>
2019-05-19 17:57:14 +1000

    chore: setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/__tests__/**/*.spec.ts',
+    '!src/**/__mocks__/**/*.ts',
+  ],
+  coverageReporters: ['lcov', 'text-summary'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+};
---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,7 @@
+import { index } from '..';
+
+describe('thing', () => {
+  it('should do something', () => {
+    expect(index).toBeTruthy();
+  });
+});
